9e15d7c1a7babdde0d9a0c196b4a9671fbe5989b
Rafkarimov <32112819+Rafkarimov@users.noreply.github.com>
2022-05-06 16:42:17 +0300

    com G2.0

---
+++ src/main/java/glava01/HelloWorldWithCoomandLine.java
@@ -0,0 +1,12 @@
+package glava01;
+
+public class HelloWorldWithCoomandLine {
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("Hello World!");
+        }
+    }
+}
