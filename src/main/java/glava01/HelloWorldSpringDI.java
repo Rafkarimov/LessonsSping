9e21d8e7bb35aa337b9e8200f59a2a2b2c333cc0
Rafkarimov <32112819+Rafkarimov@users.noreply.github.com>
2022-05-06 17:59:28 +0300

    Spring-G2.2

---
+++ src/main/java/glava01/HelloWorldSpringDI.java
@@ -0,0 +1,11 @@
+//package glava01;
+//
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//public class HelloWorldSpringDI {
+//
+//    public static void main(String[] args) {
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
+//
+//    }
+//}
